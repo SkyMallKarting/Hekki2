73d3634e895210753fd46aaadec59af747ce674f
MrFreuden <mrfreuden@gmail.com>
2022-10-18 00:33:08 +0300

    test

--- Hekki2/Form1.Designer.cs
+++ Hekki2/Form1.Designer.cs
@@ -39,9 +39,10 @@
             // 
             // sprintButton
             // 
-            this.sprintButton.Location = new System.Drawing.Point(76, 184);
+            this.sprintButton.Location = new System.Drawing.Point(101, 226);
+            this.sprintButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.sprintButton.Name = "sprintButton";
-            this.sprintButton.Size = new System.Drawing.Size(179, 75);
+            this.sprintButton.Size = new System.Drawing.Size(239, 92);
             this.sprintButton.TabIndex = 0;
             this.sprintButton.Text = "Sprint";
             this.sprintButton.UseVisualStyleBackColor = true;
@@ -49,9 +50,10 @@
             // 
             // juniorButton
             // 
-            this.juniorButton.Location = new System.Drawing.Point(294, 184);
+            this.juniorButton.Location = new System.Drawing.Point(392, 226);
+            this.juniorButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.juniorButton.Name = "juniorButton";
-            this.juniorButton.Size = new System.Drawing.Size(179, 75);
+            this.juniorButton.Size = new System.Drawing.Size(239, 92);
             this.juniorButton.TabIndex = 1;
             this.juniorButton.Text = "Junior";
             this.juniorButton.UseVisualStyleBackColor = true;
@@ -59,9 +61,10 @@
             // 
             // everyButton
             // 
-            this.everyButton.Location = new System.Drawing.Point(514, 184);
+            this.everyButton.Location = new System.Drawing.Point(685, 226);
+            this.everyButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.everyButton.Name = "everyButton";
-            this.everyButton.Size = new System.Drawing.Size(179, 75);
+            this.everyButton.Size = new System.Drawing.Size(239, 92);
             this.everyButton.TabIndex = 2;
             this.everyButton.Text = "Every On Every";
             this.everyButton.UseVisualStyleBackColor = true;
@@ -69,9 +72,10 @@
             // 
             // numbersOfKarts
             // 
-            this.numbersOfKarts.Location = new System.Drawing.Point(88, 292);
+            this.numbersOfKarts.Location = new System.Drawing.Point(117, 359);
+            this.numbersOfKarts.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numbersOfKarts.Name = "numbersOfKarts";
-            this.numbersOfKarts.Size = new System.Drawing.Size(100, 146);
+            this.numbersOfKarts.Size = new System.Drawing.Size(132, 179);
             this.numbersOfKarts.TabIndex = 3;
             this.numbersOfKarts.Text = "";
             this.numbersOfKarts.TextChanged += new System.EventHandler(this.numbersOfKarts_TextChanged);
@@ -79,17 +83,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(85, 276);
+            this.label1.Location = new System.Drawing.Point(113, 340);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(147, 13);
+            this.label1.Size = new System.Drawing.Size(186, 16);
             this.label1.TabIndex = 4;
             this.label1.Text = "Номера картов, через enter";
             // 
             // Cherkasy
             // 
-            this.Cherkasy.Location = new System.Drawing.Point(76, 63);
+            this.Cherkasy.Location = new System.Drawing.Point(101, 78);
+            this.Cherkasy.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Cherkasy.Name = "Cherkasy";
-            this.Cherkasy.Size = new System.Drawing.Size(179, 75);
+            this.Cherkasy.Size = new System.Drawing.Size(239, 92);
             this.Cherkasy.TabIndex = 5;
             this.Cherkasy.Text = "Черкасы";
             this.Cherkasy.UseVisualStyleBackColor = true;
@@ -97,9 +103,10 @@
             // 
             // schoolButton
             // 
-            this.schoolButton.Location = new System.Drawing.Point(294, 63);
+            this.schoolButton.Location = new System.Drawing.Point(755, 78);
+            this.schoolButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.schoolButton.Name = "schoolButton";
-            this.schoolButton.Size = new System.Drawing.Size(179, 75);
+            this.schoolButton.Size = new System.Drawing.Size(239, 92);
             this.schoolButton.TabIndex = 6;
             this.schoolButton.Text = "School";
             this.schoolButton.UseVisualStyleBackColor = true;
@@ -107,9 +114,9 @@
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.schoolButton);
             this.Controls.Add(this.Cherkasy);
             this.Controls.Add(this.label1);
@@ -117,6 +124,7 @@
             this.Controls.Add(this.everyButton);
             this.Controls.Add(this.juniorButton);
             this.Controls.Add(this.sprintButton);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Main";
             this.Text = "Form1";
             this.ResumeLayout(false);
